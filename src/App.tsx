6f6e587e9d188416c5910c02b4a7a7191a8eb0bc
sebastianrodriguez <sebrod35@purdue.edu>
2025-04-06 01:38:16 -0400

    more changes made to the starting page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavigationBar from './components/NavigationBar';
 
 function App() {
   return (
     <div className="App">
+      <NavigationBar></NavigationBar>
       <h1 className="Title">Welcome to</h1>
       <h1 className="secondTitle">The Reading Room</h1>
     </div>
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../Styles/NavigationBarStyles.css'
 
 function NavigationBar() {
     return (
         <div className="mainNavigationBar">
-            <button>Trending Books</button>
+            <button className="bookNavigator">Trending Books</button>
             <button>Sign Up</button>
             <button>Log In</button>
         </div>
